52aa2e871617543c73b9abbc64beeef0a09d468b
apelsin16 <apelsin@i.ua>
2023-04-09 16:21:42 +0300

    ready

--- src/blocks/modules/main/main.js
+++ src/blocks/modules/main/main.js
@@ -15,6 +15,51 @@
         }
     }
 
+    function create(tag, cls, content ) {
+        const element = document.createElement(tag);
+        cls.split(" ").forEach(el =>  element.classList.add(el));
+        if(content) element.innerHTML = content;
+        return element
+    }
+
+    function createCard(text, author, url) {
+
+        const cardWrapper = create("div", "col-12 col-md-6");
+        const card = create("div", "card");
+
+        const canvas = create("canvas", "card-img-top");
+
+        const img = new Image();
+        img.crossOrigin = "anonymous";
+        img.src = url;
+        img.onload = function () {
+            canvas.width = this.width;
+            canvas.height = this.height;
+            canvas.getContext("2d").drawImage(this, 0, 0);
+        }
+
+        const cardBody = create("div", "card-body");
+        const cardTitle = create("h5", "card-title ms-3", author);
+        const cardText = create("p", "card-text custom-text ms-3", text);
+        const cardFooter = create("div", "card-footer p-4");
+        const buttonSave = create("button", "btn btn-success mx-1", "Save to collection");
+        const buttonShare = create("button", "btn btn-outline-secondary", "Share");
+
+        cardBody.append(cardTitle);
+        cardBody.append(cardText);
+
+        cardFooter.append(buttonSave);
+        cardFooter.append(buttonShare);
+
+        card.append(canvas);
+        card.append(cardBody);
+        card.append(cardFooter);
+
+        cardWrapper.append(card);
+
+        return cardWrapper
+    }
+
     document.addEventListener("DOMContentLoaded", () => {
         fetch(url)
             .then((response) => {
@@ -23,19 +68,22 @@
             .then((data) => {
                 data.map(el => {
                     cardText = lorem.slice(0, lorem.length * Math.random()/5);
-                    let card = `<div class="col-12 col-md-6">
-                                    <div class="card"><img class="card-img-top" src="${el.url}">
-                                      <div class="card-body">
-                                        <h5 class="card-title ms-3">${el.author}</h5>
-                                        <p class="card-text custom-text ms-3">${cardText}</p>                                        
-                                      </div>
-                                      <div class="card-footer p-4">
-                                        <button class="btn btn-success">Save to collection</button>
-                                        <button class="btn btn-outline-secondary">Share</button>
-                                      </div>
-                                    </div>
-                                  </div>`;
-                    cards.innerHTML += card;
+                    const card = createCard(cardText, el.author, el.url);
+                    console.log(card);
+                    cards.append(card);
+                    // let card = `<div class="col-12 col-md-6">
+                    //                 <div class="card"><img class="card-img-top" src="${el.url}" >
+                    //                   <div class="card-body">
+                    //                     <h5 class="card-title ms-3">${el.author}</h5>
+                    //                     <p class="card-text custom-text ms-3">${cardText}</p>
+                    //                   </div>
+                    //                   <div class="card-footer p-4">
+                    //                     <button class="btn btn-success">Save to collection</button>
+                    //                     <button class="btn btn-outline-secondary">Share</button>
+                    //                   </div>
+                    //                 </div>
+                    //               </div>`;
+                    // cards.innerHTML += card;
                 });
             }).then(() => {
                 const cardTexts = document.querySelectorAll(".card-text");
